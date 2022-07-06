206db7e72a94ee40e335fdb01f76152bee491dd0
nicktianboli <li_tianbo@live.com>
2022-07-06 20:39:48 +0800

    Pople basis set. Ref sail/aisci/jdft#9

--- jdft/orbitals.py
+++ jdft/orbitals.py
@@ -4,14 +4,8 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 from jax.experimental import sparse
-from jdft.functions import decov
+from jdft.functions import decov, factorial
 # from scipy.special import factorial
-
-
-def factorial(x):
-  """Calculate the factorial of x."""
-  x = jnp.asarray(x, dtype=jnp.float32)
-  return jnp.exp(jax.lax.lgamma(x + 1))
 
 
 def ao_label_parser(ao_label):
@@ -239,7 +233,7 @@ class GTO(Basis):
     return const * output
 
 
-class Pople(Basis):
+class PopleSparse(Basis):
   """The pople basis set."""
 
   def __init__(self, pyscf_mol):
@@ -287,6 +281,146 @@ class Pople(Basis):
 
   def overlap(self, intor=None):
     """Compute overlap matrix for Pople basis."""
+    if not intor:
+      return self.pyscf_mol.intor('int1e_ovlp_sph')
+    else:
+      intor.mo = self.__call__
+      return intor.overlap()
+
+
+class Pople(Basis):
+  def __init__(self, pyscf_mol):
+    super().__init__()
+    self.pyscf_mol = pyscf_mol
+    self.elements = pyscf_mol.elements
+    self.atom_coords = pyscf_mol.atom_coords()
+    self._basis = pyscf_mol._basis
+    self.basis_max = len(self._basis[pyscf_mol.elements[0]][0])-1
+    self.ao_labels = pyscf_mol.ao_labels()
+
+    exponent_mat = []
+    coeff_mat = []
+
+    self.ijk = []
+    self.coord = []
+
+    for idx in np.arange(len(self.elements)):
+      element = self.elements[idx]
+      coord = self.atom_coords[idx]
+      for i in self._basis[element]:
+        if i[0] == 0:
+          self.ijk.append([0, 0, 0])
+          self.coord.append(coord)
+          prm_array = np.array(i[1:])
+          exponent_mat.append(prm_array[:, 0])
+          coeff_mat.append(prm_array[:, 1])
+
+        if i[0] == 1:
+          self.ijk.append([1, 0, 0])
+          self.ijk.append([0, 1, 0])
+          self.ijk.append([0, 0, 1])
+          self.coord += [coord] * 3
+
+          prm_array = np.array(i[1:])
+          exponent_mat += [prm_array[:, 0]] * 3
+          coeff_mat += [prm_array[:, 1]] * 3
+
+        if i[0] == 2:
+          raise NotImplementedError('d orbital has not been implemented yet.')
+
+    self.ijk = np.array(self.ijk)
+    self.coord = np.array(self.coord)
+
+    self.exponent_mat = np.zeros([len(exponent_mat), self.basis_max])
+    for p, q in enumerate(exponent_mat):
+      self.exponent_mat[p][0:len(q)] = q
+
+    self.coeff_mat = np.zeros([len(coeff_mat), self.basis_max])
+    for p, q in enumerate(coeff_mat):
+      self.coeff_mat[p][0:len(q)] = q
+
+  def __call__(self, r):
+    '''
+    Args:
+      |r: shape [G, 3] where G is the number of grids.
+
+    Returns:
+      |shape: [G, num_ao], where num_ao is the number of atomic orbitals.
+    '''
+
+    const1 = (2 * self.exponent_mat / np.pi)**(3 / 4)
+    const2 = jnp.sum(self.ijk, axis=1, keepdims=True)
+    const3 = jax.vmap(lambda x: factorial(x))(self.ijk.T)
+    const3 = jnp.prod(const3.T, axis=1, keepdims=True)
+    const4 = jax.vmap(lambda x: factorial(2 * x))(self.ijk.T)
+    const4 = jnp.prod(const4.T, axis=1, keepdims=True)
+    const = const1 * (((8 * self.exponent_mat) ** const2 * const3) /
+            const4) ** 0.5
+    output = const * jnp.prod(
+      jnp.power(r-self.coord, self.ijk),
+      axis=1,
+      keepdims=True
+      )
+    def fc(c): return jnp.linalg.norm(r - c) ** 2
+
+    output *= self.coeff_mat * jnp.exp(
+      -self.exponent_mat * jnp.expand_dims(jax.vmap(fc)(self.coord), 1)
+      )
+    return jnp.sum(output, axis=1)
+
+  def overlap(self, intor=None):
+    if not intor:
+      return self.pyscf_mol.intor('int1e_ovlp_sph')
+    else:
+      intor.mo = self.__call__
+      return intor.overlap()
+
+
+class PopleFast(Basis):
+
+  # this is a Pople type atomic orbital class.
+
+  def __init__(self, pyscf_mol):
+    self.pyscf_mol = pyscf_mol
+    self.atom_coords = pyscf_mol.atom_coords()
+    self._basis = pyscf_mol._basis
+    self.elements = pyscf_mol.elements
+
+  def __call__(self, r):
+    '''
+    R^3 -> R^N. N-body atomic orbitals.
+    input:
+        (N: the number of atomic orbitals.)
+        |r: (3) coordinate.
+    '''
+    output = []
+    for idx in np.arange(len(self.elements)):
+      element = self.elements[idx]
+      coord = self.atom_coords[idx]
+      for i in self._basis[element]:
+        if i[0] == 0:
+          prm_array = jnp.array(i[1:])
+          output.append(
+            jnp.sum(
+              prm_array[:, 1] *
+              jnp.exp(-prm_array[:, 0] * jnp.linalg.norm(r - coord)**2) *
+              (2 * prm_array[:, 0] / jnp.pi)**(3 / 4)
+            )
+          )
+
+        elif i[0] == 1:
+          prm_array = jnp.array(i[1:])
+          output += [
+            (r[j] - coord[j]) * jnp.sum(
+              prm_array[:, 1] *
+              jnp.exp(-prm_array[:, 0] * jnp.linalg.norm(r - coord)**2) *
+              (2 * prm_array[:, 0] / jnp.pi)**(3 / 4) *
+              (4 * prm_array[:, 0])**0.5
+            ) for j in np.arange(3)
+          ]
+    return jnp.array(output)
+
+  def overlap(self, intor=None):
     if not intor:
       return self.pyscf_mol.intor('int1e_ovlp_sph')
     else:
