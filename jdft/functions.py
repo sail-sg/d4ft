4ed2e161c68c403c6aee632389d9f4afac69a4cf
nicktianboli <li_tianbo@live.com>
2022-08-25 16:22:42 +0800

    yapf

--- jdft/functions.py
+++ jdft/functions.py
@@ -9,7 +9,7 @@ import numpy as np
 
 def euclidean_distance(x, y):
   """Euclidean distance."""
-  return jnp.sqrt(jnp.sum((x - y)**2 + 1e-10))
+  return jnp.sqrt(jnp.sum((x - y)**2 + 1e-18))
 
 
 def factorial(x):
@@ -57,19 +57,25 @@ def gaussian_intergral(alpha, n):
   #     raise NotImplementedError()
 
   return (
-      (n == 0) * jnp.sqrt(jnp.pi / alpha) +
-      (n == 2) * 1 / 2 / alpha * jnp.sqrt(jnp.pi / alpha) +
-      (n == 4) * 3 / 4 / alpha**2 * jnp.sqrt(jnp.pi / alpha) +
-      (n == 6) * 15 / 8 / alpha**3 * jnp.sqrt(jnp.pi / alpha)
+    (n == 0) * jnp.sqrt(jnp.pi / alpha) +
+    (n == 2) * 1 / 2 / alpha * jnp.sqrt(jnp.pi / alpha) +
+    (n == 4) * 3 / 4 / alpha**2 * jnp.sqrt(jnp.pi / alpha) +
+    (n == 6) * 15 / 8 / alpha**3 * jnp.sqrt(jnp.pi / alpha)
   )
 
 
 def decov(cov):
   """Decomposition of covariance matrix."""
   v, u = jnp.linalg.eigh(cov)
-  v = jnp.diag(jnp.real(v)**(-1 / 2)) + jnp.eye(v.shape[0]) * 1e-10
+  v = jnp.diag(jnp.real(v)**(-1 / 2)) + jnp.eye(v.shape[0]) * 1e-7
   ut = jnp.real(u).transpose()
   return jnp.matmul(v, ut)
+
+
+# def decov(cov):
+#   l = jnp.linalg.cholesky(cov)
+#   l = jnp.linalg.inv(l)
+#   return l
 
 
 def set_diag_zero(x):
@@ -96,12 +102,12 @@ def minibatch_vmap(f, in_axes=0, batch_size=10):
 
     def _process_batch(start_index):
       batch_args = (
-          jax.lax.dynamic_slice_in_dim(
-              a,
-              start_index=start_index,
-              slice_size=batch_size,
-              axis=ax,
-          ) if ax is not None else a for a, ax in zip(args, in_axes)
+        jax.lax.dynamic_slice_in_dim(
+          a,
+          start_index=start_index,
+          slice_size=batch_size,
+          axis=ax,
+        ) if ax is not None else a for a, ax in zip(args, in_axes)
       )
       return batch_f(*batch_args)
 
