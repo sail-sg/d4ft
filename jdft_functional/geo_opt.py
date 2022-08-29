6eda91b3ff1cfcd5fdea49bf6bdf37465c0795d9
nicktianboli <li_tianbo@live.com>
2022-08-29 11:07:41 +0800

    temp save

--- jdft_functional/geo_opt.py
+++ jdft_functional/geo_opt.py
@@ -4,9 +4,13 @@ import jax
 import jax.numpy as jnp
 from energy import energy_gs
 import optax
-from grids import _gen_grid
+from grids import _gen_grid, _grid_shift
 from jdft.functions import distmat
 import copy
+
+from jax.config import config
+
+config.update("jax_debug_nans", True)
 
 
 def geo_opt(
@@ -16,84 +20,136 @@ def geo_opt(
   seed=123,
   converge_threshold=1e-3,
   batch_size=1000,
+  momentum=0.1
 ):
   """Run the main training loop."""
-  params = mol._init_param(seed)
-  print(params)
 
   nuclei = mol.nuclei
-  optimizer = optax.adam(lr)
-  opt_state = optimizer.init((params[0], nuclei['loc']))
-
-  if batch_size > mol.grids.shape[0]:
-    batch_size = mol.grids.shape[0]
 
   params_mask = jnp.ones_like(nuclei['loc'])
   params_mask = params_mask.at[:, 0].set(0)
   params_mask = params_mask.at[:, 1].set(0)
+  params_mask = params_mask.at[0, :].set(0)
+
+  bond_length = jnp.zeros([1, 1]) + nuclei['loc'][-1, -1]
+
+  params = (*mol._init_param(seed), bond_length)
+  print(params)
+
+  schedule = optax.warmup_cosine_decay_schedule(
+    init_value=0.00001,
+    peak_value=0.01,
+    warmup_steps=100,
+    decay_steps=epoch / 2,
+    end_value=lr,
+  )
+
+  optimizer = optax.chain(
+    optax.clip(1.0),
+    optax.adam(learning_rate=schedule),
+  )
+
+  # optimizer = optax.adam(lr)
+  opt_state = optimizer.init(params)
+
+  _grids, _weights, _atoms = _gen_grid(mol.pyscf_mol, level=1, atom_label=True)
 
   @jax.jit
   def update(params, nuclei, opt_state, batch):  # params only contains mo.
-
-    mo_params, _ = params
-    _params = (mo_params, nuclei['loc'])
     g, w = batch
 
-    def loss(_params):
-      mo_params, atom_coords = _params
-      atom_coords *= params_mask
+    def loss(params):
+      mo_params, ao_params, bond_length = params
+      atom_coords = nuclei['loc'] * (1 - params_mask) \
+        + params_mask * bond_length
+
+      # _grids = _grid_shift(g, _atoms, nuclei['loc'], atom_coords)
 
       def mo(r):
-        return mol.mo((mo_params, None), r, grids=g, weights=w) * mol.nocc
+        return mol.mo(
+          (mo_params, ao_params),
+          r,
+          grids=g,
+          weights=w,
+          atom_coords=atom_coords
+        ) * mol.nocc
 
-      nuclei['loc'] = copy.deepcopy(atom_coords)
-      return energy_gs(mo, nuclei, batch, batch)
+      nuclei_new = {'loc': atom_coords, 'charge': nuclei['charge']}
+      # nuclei_new = nuclei
 
-    (e_total, e_splits), grad = jax.value_and_grad(loss, has_aux=True)(_params)
+      e_total, e_splits = energy_gs(mo, nuclei_new, batch, batch)
+      return e_total, (e_splits, nuclei_new)
+
+    (e_total, aux), grad = jax.value_and_grad(
+      loss, argnums=0, has_aux=True
+    )(
+      params
+    )
+
+    e_splits, nuclei_new = aux
     updates, opt_state = optimizer.update(grad, opt_state)
-    _params = optax.apply_updates(_params, updates)
-    nuclei['loc'] = copy.deepcopy(_params[1])
-    params = (_params[0], _)
-    return params, nuclei, opt_state, (e_total, *e_splits)
+    params = optax.apply_updates(params, updates)
+    # nuclei = {'loc': momentum * nuclei['loc'] + (1-momentum) * _params[1],
+    # 'charge': nuclei['charge']}
+    # nuclei['loc'] = momentum * nuclei['loc'] + (1-momentum) * _params[1]
+    return params, nuclei_new, opt_state, (e_total, *e_splits)
 
-  logging.info(f"Starting... Random Seed: {seed}, Batch size: {batch_size}")
+  logging.info(f"Starting... Random Seed: {seed}")
 
   prev_loss = 0.
   start_time = time.time()
   e_train = []
   converged = False
+  nuclei_old = copy.deepcopy(nuclei)
+  nuclei_new = copy.deepcopy(nuclei)
 
-  logging.info(f"Batch size: {batch_size}")
   logging.info(f"Total grid points: {len(mol.grids)}")
 
   for i in range(epoch):
     Es_batch = []
 
-    _grids, _weights = _gen_grid(
-      mol.pyscf_mol, level=1, atom_label=False, atom_coords=nuclei['loc']
-    )
+    # _grids, _weights = _gen_grid(
+    #   mol.pyscf_mol, level=mol.level, atom_label=False,
+    # atom_coords=nuclei['loc']
+    # )
+
     # this can be replaced by just shifting the grids according to the new atom
     # coordinates, instead of resampling.
 
+    _grids = _grid_shift(_grids, _atoms, nuclei_old['loc'], nuclei_new['loc'])
     batch = (_grids, _weights)
-    params, nuclei, opt_state, Es = update(params, nuclei, opt_state, batch)
-    Es_batch.append(Es)
+    # batches = batch_sampler(_grids, _weights, batch_size, seed=seed+i)
+    # batch = (mol.grids, mol.weights)
 
+    # for batch in batches:
+    nuclei_old = copy.deepcopy(nuclei_new)
+    params, nuclei_new, opt_state, Es = update(
+      params, nuclei_old, opt_state, batch
+    )
+    Es_batch.append(Es)
+    # mol.mo.ao.atom_coords = nuclei_new['loc']
     # retrieve all energy terms
     e_total, e_kin, e_ext, e_xc, e_hartree, e_nuc = jnp.mean(
       jnp.array(Es_batch), axis=0
     )
-    print(e_total, e_kin, e_ext, e_xc, e_hartree, e_nuc)
+
+    if jnp.isnan(e_total):
+      print(params)
+      print(nuclei_old)
+      print(nuclei_new)
+      print(e_total, e_kin, e_ext, e_xc, e_hartree, e_nuc)
+      print(_grids)
+      break
     # track total energy for convergence check
     e_train.append(e_total)
 
-    if (i + 1) % 1 == 0:
-      atom_coords = nuclei['loc']
+    if (i + 1) % 10 == 0:
+      print(e_total, e_kin, e_ext, e_xc, e_hartree, e_nuc)
       logging.info(f"Iter: {i+1}/{epoch}. Ground State Energy: {e_total:.3f}.")
-      logging.info(f"current coords: {nuclei['loc'].tolist()}")
-      logging.info(f'{distmat(atom_coords)*0.529177249}')
+      logging.info(f"current coords: {nuclei_new['loc'].tolist()}")
+      logging.info(f"{distmat(nuclei_new['loc'])*0.529177249}")
 
-    if jnp.abs(prev_loss - e_total) < converge_threshold:
+    if jnp.abs(prev_loss - e_total) < converge_threshold - 1:
       converged = True
       break
     else:
@@ -122,14 +178,16 @@ if __name__ == "__main__":
 
   h2_start = """
   H 0.0000 0.0000 0.0000;
-  H 0.0000 0.0000 1.2;
+  H 0.0000 0.0000 0.6;
   """
+  print(h2_start)
 
-  mol = molecule(h2_start, spin=0, level=2, basis="6-31g", mode='go')
+  mol = molecule(h2_start, spin=0, level=3, basis="6-31g", mode='go')
   geo_opt(
     mol,
-    epoch=200,
-    lr=4e-3,
+    epoch=5000,
+    lr=2e-4,
+    seed=15789,
     batch_size=50000,
-    converge_threshold=1e-7,
+    converge_threshold=1e-8,
   )
