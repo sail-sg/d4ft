fbdb5b553a1eb94ea7ce35b0f5c84f898d8e827d
linmin <mavenlin@gmail.com>
2023-05-02 19:23:56 +0800

    add comments

--- d4ft/integral/gto/gto_utils.py
+++ d4ft/integral/gto/gto_utils.py
@@ -19,6 +19,7 @@ perm_2n_n = jnp.array(scipy.special.perm(2 * _r25, _r25))
  Used in GTO normalization and OS horizontal recursion."""
 
 
+# TODO: is it good to vmap here?
 @jax.vmap
 def normalization_constant(
   angular: Int[Array, "*batch 3"], exponent: Float[Array, "*batch"]
@@ -32,6 +33,7 @@ def normalization_constant(
   )
 
 
+# TODO: GTO or Gto?
 class GTOParam(NamedTuple):
   angular: Int[Array, "*batch 3"]
   """angular momentum vector, e.g. (0,1,0)"""
@@ -53,22 +55,38 @@ class GTO(NamedTuple):
   """GTO exponent / bandwith"""
   coeff: Float[Array, "*batch"]
   """GTO contraction coefficient"""
+  # TODO: it should be more clear that this coeff is only for making AO,
+  # but not MO as the shape here is only *batch.
+  # TODO: BTW, should we call it batch, or num_gto?
+  # TODO: shouldn't this class be called LCGTO? if coeff is one of the fields?
+
   sto_to_gto: Union[Int[Array, "*n_stos"], tuple]
   """GTO segment lengths. e.g. (3,3) for H2 in sto-3g.
   Store it in tuple form so that it is hashable, and can be
   passed to a jitted function as static arg."""
+  # TODO: this is not necessarily sto? It could be any LCGTO to make any orbital.
+  # including the cc orbitals.
+  # TODO: naming sto_to_gto, should we call it sto_splits? or sto_seg_len?
+  # naming can be borrowed from the segment_sum API in jax.
+
   atom_to_gto: Int[Array, "*n_atoms"]
+  # TODO: same here for the naming
   charge: Int[Array, "*batch"]
   """charges of the atoms"""
+  # TODO: the batch here could be confusing, we should distinguish it from
+  # the batch above. One is for num_gtos, the other is for the num_atoms.
   sto_seg_id: Int[Array, "n_gtos"]
   """for contracting GTO tensor to AO/STO basis."""
   N: Int[Array, "nmo_or_ngto"]
   """Store computed normalization constant."""
 
+  # TODO: This N seems stateful
+
   def params(self):
     return [self.angular, self.center, self.exponent, self.coeff]
 
   def map_params(self, f: Callable) -> Tuple[GTOParam, Float[Array, "*batch"]]:
+    # TODO: add purpose and use example for this API.
     angular, center, exponent, coeff = map(f, self.params())
     return GTOParam(angular, center, exponent), coeff
 
@@ -88,6 +106,11 @@ class GTO(NamedTuple):
     return jax.ops.segment_sum(gto_val, self.sto_seg_id, n_stos)
 
 
+# TODO: The API design of tf Dataset could be borrowed here
+# tf.dataset.Dataset.from_generator/from_tensor_slices
+# correspondingly, we can set this function as the static member of GTO.
+# GTO.from_pyscf_mol
+# GTO.from_string
 def mol_to_gto(mol: pyscf.gto.mole.Mole) -> GTO:
   """Transform pyscf mol object to GTOs.
 
@@ -120,6 +143,21 @@ def mol_to_gto(mol: pyscf.gto.mole.Mole) -> GTO:
   n_gto = sum(sto_to_gto)
   logging.info(f"there are {n_gto} GTOs")
   return gtos
+
+
+# TODO: As per our last discussion, we could use the more native hk way
+# There are two different gto object we need, one is initialized from pyscf,
+# the parameters are fixed, we can construct from pyscf.
+# Another is optimizable, we want it to be hk.transformed.
+
+# Design 1: we have one GTO, and another `d4ft.hk.GTO` (naming can be improved).
+# gto = GTO.from_pyscf_mol(mol)
+# hk_gto = d4ft.hk.GTO(init=gto, use_exponent=True, use_center=True)
+
+# Design 2: we only have one GTO, but we control its behavior via arguments.
+# gto = GTO.from_pyscf_mol(mol, use_exponent=True, use_center=True)
+# If any use_* is true, then this can be only constructed within a hk.transformed
+# context.
 
 
 def get_gto_param_fn(mol: pyscf.gto.mole.Mole) -> hk.Transformed:
