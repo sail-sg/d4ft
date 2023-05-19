5a9b6e54b817e380dd959c621f1df38b4158fa09
Zekun Shi <shizk@sea.com>
2023-05-19 09:34:36 +0800

    add license

--- tests/integral/pw/pw_test.py
+++ tests/integral/pw/pw_test.py
@@ -1,5 +1,18 @@
-from functools import partial
+# Copyright 2023 Garena Online Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import haiku as hk
 import numpy as np
 from absl import logging
 from absl.testing import absltest, parameterized
@@ -7,7 +20,6 @@ from d4ft.hamiltonian.ortho import qr_factor
 from d4ft.integral.pw.pw import PW
 from d4ft.system.crystal import Crystal
 from d4ft.utils import to_3dvec
-import haiku as hk
 
 
 class PWTest(parameterized.TestCase):
