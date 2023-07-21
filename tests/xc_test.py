1fa4d703435fa5fc1b9398be1fef482aae76e459
Zekun Shi <shizk@sea.com>
2023-07-21 21:55:48 +0800

    add density test

--- tests/xc_test.py
+++ tests/xc_test.py
@@ -21,6 +21,7 @@ import jax_xc
 from absl import logging
 from absl.testing import absltest, parameterized
 
+from d4ft.integral.quadrature.utils import wave2density
 from d4ft.config import get_config
 from d4ft.hamiltonian.ortho import qr_factor, sqrt_inv
 from d4ft.integral.gto.cgto import CGTO
@@ -28,6 +29,7 @@ from d4ft.integral.quadrature.grids import DifferentiableGrids
 from d4ft.system.mol import Mol, get_pyscf_mol
 from d4ft.utils import compose
 from d4ft.xc import get_xc_intor
+from d4ft.types import Fock, MoCoeff, QuadGridsNWeights
 
 
 class XCTest(parameterized.TestCase):
@@ -82,7 +84,25 @@ class XCTest(parameterized.TestCase):
     logging.info(e_xc)
     self.assertFalse(jnp.isnan(e_xc))
 
+    # test point
+    r1 = grids_and_weights[0][0]
+
+    def density_fn(mo_coeff: MoCoeff):
+      mo_fn = lambda r: mo_coeff @ cgto.eval(r)
+      density = wave2density(mo_fn, True)
+      return jnp.sum(density(r1))
+
+    mo_density_fn = hk.without_apply_rng(
+      hk.transform(compose(density_fn, mo_coeff_fn))
+    )
+    n_r = mo_density_fn.apply(params)
+    logging.info(n_r)
+
     # test gradient
+    n_grads = jax.grad(mo_density_fn.apply)(params)
+    logging.info(n_grads)
+    self.assertFalse(jnp.isnan(n_grads["~"]["mo_params"]).any())
+
     xc_grads = jax.grad(mo_xc_fn.apply)(params)
     logging.info(xc_grads)
     self.assertFalse(jnp.isnan(xc_grads["~"]["mo_params"]).any())
