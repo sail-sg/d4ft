af0dd138aeaeace03e5b8555384290fb22c4c51e
Zekun Shi <shizk@sea.com>
2023-08-10 18:06:04 +0800

    fix lint

--- tests/integral/gto/primitive_test.py
+++ tests/integral/gto/primitive_test.py
@@ -19,45 +19,7 @@ import numpy as np
 from absl.testing import absltest, parameterized
 
 from d4ft.integral.gto.cgto import CGTO
-from d4ft.system.mol import Mol
-
-
-def eval_ao(r: jax.Array, mol: Mol):
-  """Evaluate N-body atomic orbitals at location r.
-
-  Args:
-        r: (3) coordinate.
-
-  Returns:
-    (N,) ao output
-  """
-  atom_coords = mol.atom_coords
-  output = []
-  for idx in np.arange(len(mol.elements)):
-    element = mol.elements[idx]
-    coord = atom_coords[idx]
-    for i in mol.basis[element]:
-      prm_array = jnp.array(i[1:])
-      exponents = prm_array[:, 0]
-      coeffs = prm_array[:, 1]
-
-      if i[0] == 0:  # s-orbitals
-        output.append(
-          jnp.sum(
-            coeffs * jnp.exp(-exponents * jnp.sum((r - coord)**2)) *
-            (2 * exponents / jnp.pi)**(3 / 4)
-          )
-        )
-
-      elif i[0] == 1:  # p-orbitals
-        output += [
-          (r[j] - coord[j]) * jnp.sum(
-            coeffs * jnp.exp(-exponents * jnp.sum((r - coord)**2)) *
-            (2 * exponents / jnp.pi)**(3 / 4) * (4 * exponents)**0.5
-          ) for j in np.arange(3)
-        ]
-
-  return jnp.array(output)
+from d4ft.system.mol import Mol, get_pyscf_mol
 
 
 class HKTest(parameterized.TestCase):
@@ -67,10 +29,16 @@ class HKTest(parameterized.TestCase):
     ("o", "sto-3g", 1, 5, 15),
   )
   def test_basis_param(self, system, basis, n_atoms, n_cgtos, n_pgtos):
+    pyscf_mol = get_pyscf_mol(system, basis)
+
     mol = Mol.from_mol_name(system, basis)
-    cgto_transformed = hk.without_apply_rng(
-      hk.transform(lambda: CGTO.from_mol(mol).to_hk())
-    )
+
+    @hk.without_apply_rng
+    @hk.transform
+    def cgto_transformed():
+      cgto_cart = CGTO.from_mol(mol)
+      cgto = CGTO.from_cart(cgto_cart)
+      return cgto.to_hk()
 
     gparams = cgto_transformed.init(1)
     cgto = cgto_transformed.apply(gparams)
@@ -80,10 +48,10 @@ class HKTest(parameterized.TestCase):
       ao_val = jax.jit(cgto.eval)(r)
       print(ao_val)
 
-      ao_val_exp = eval_ao(r, mol)
+      ao_val_exp = pyscf_mol.eval_ao("GTOval_sph", r[None, :])
       print(ao_val_exp)
 
-      self.assertTrue(jnp.allclose(ao_val, ao_val_exp, atol=1e-6))
+      self.assertTrue(jnp.allclose(ao_val, ao_val_exp))
 
     self.assertEqual(len(ao_val), n_cgtos)
 
