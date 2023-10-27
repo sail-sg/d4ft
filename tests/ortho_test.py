4e65f715e420d6d630c0809c4a7bf9e0ab73c66a
Zekun Shi <shizk@sea.com>
2023-10-27 09:37:25 +0800

    fix test

--- tests/ortho_test.py
+++ tests/ortho_test.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import jax
+
+jax.config.update("jax_enable_x64", True)
 import numpy as np
 import scipy
 from absl import logging
